14b05a7d54a7568d38178f1a3393220ccbd648e0
Rocco Moresi <137230323+roccomoresi@users.noreply.github.com>
2024-09-15 21:01:15 -0300

    equipo agregado

---
+++ src/main/java/org/example/products/Equipo.java
@@ -0,0 +1,15 @@
+package org.example.products;
+
+public class Equipo {
+    protected String nombre;
+    protected int tiempoTotalEquipo;
+
+    Ciclista contrarelojista = new Contrarelojista();
+    Ciclista escalador = new Escalador();
+    Ciclista velocista = new Velocista();
+
+
+    public Equipo(int idCiclista, int tiempoTotalEquipo) {
+
+    }
+}
